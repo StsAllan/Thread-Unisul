7014e8e37a5794db3e63f82c7612b6fe47897440
Allan dos Santos <116950814+StsAllan@users.noreply.github.com>
2023-03-21 19:55:27 -0300

    Add files via upload

---
+++ src/thread_unisul/PrincipalThread.java
@@ -0,0 +1,10 @@
+
+package thread_unisul;
+
+public class PrincipalThread {
+public static void main(String[] args) {
+System.out.println("Numero de threads:" + 
+Thread.activeCount());
+System.out.println(Thread.currentThread());
+}
+}
